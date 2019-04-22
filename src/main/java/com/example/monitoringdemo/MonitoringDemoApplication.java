70645a40a17e534f8fa8ef52fe8bb1dc67c6aff6
서경원/모니터링플랫폼개발팀/NE <kyeongwon.seo@nhnent.com>
2019-04-22 18:43:46 +0900

    init

--- src/main/java/com/example/monitoringdemo/MonitoringDemoApplication.java
+++ src/main/java/com/example/monitoringdemo/MonitoringDemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.monitoringdemo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
