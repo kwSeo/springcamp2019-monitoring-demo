b3cc651540af12445a5937bdf4f93864bd839b3d
서경원/모니터링플랫폼개발팀/NE <kyeongwon.seo@nhnent.com>
2019-04-25 20:18:15 +0900

    캐시 활성화

--- src/main/java/com/example/monitoringdemo/MonitoringDemoApplication.java
+++ src/main/java/com/example/monitoringdemo/MonitoringDemoApplication.java
@@ -2,10 +2,12 @@ package com.example.monitoringdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableCaching
 public class MonitoringDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonitoringDemoApplication.class, args);
