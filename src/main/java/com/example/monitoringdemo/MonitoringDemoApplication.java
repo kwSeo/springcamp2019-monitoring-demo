c8827e33e277acbd64d3908c8e18352955032fa9
서경원/모니터링플랫폼개발팀/NE <kyeongwon.seo@nhnent.com>
2019-04-25 21:43:02 +0900

    added metric binders

--- src/main/java/com/example/monitoringdemo/MonitoringDemoApplication.java
+++ src/main/java/com/example/monitoringdemo/MonitoringDemoApplication.java
@@ -1,16 +1,43 @@
 package com.example.monitoringdemo;
 
+import com.netflix.spectator.atlas.AtlasConfig;
+import io.micrometer.atlas.AtlasMeterRegistry;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
+import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableCaching
 public class MonitoringDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MonitoringDemoApplication.class, args);
+//        SpringApplication.run(MonitoringDemoApplication.class, args);
+
+//        LoggingMeterRegistry registry = new LoggingMeterRegistry();
+        PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+
+        new JvmGcMetrics().bindTo(registry);
+        new JvmMemoryMetrics().bindTo(registry);
+        new JvmThreadMetrics().bindTo(registry);
+        new ProcessorMetrics().bindTo(registry);
+        new ClassLoaderMetrics().bindTo(registry);
+
+        System.out.println(registry.scrape());
     }
 }
 
